2425101bd75d3fbc3dfcceb459830beef215023a
Shane Sprague <shanesprague89@gmail.com>
2014-08-04 20:58:28 -0400

    Added more CSS, animations, gradients

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $pageTitle = "Mikki Manco Portfolio"; ?>
+<?php $pageTitle = "Mikki Manco"; ?>
 
 <?php include("inc/header.php"); ?>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer laoreet imperdiet mauris nec vestibulum. Nunc aliquam tincidunt orci sit amet condimentum. Mauris vehicula venenatis interdum. Aliquam sit amet mi iaculis, mollis justo sit amet, luctus arcu. Nullam ut mauris ultricies velit scelerisque elementum vel porttitor nisl. Nulla vestibulum enim eget risus lacinia, a pharetra tortor imperdiet. Nam tincidunt, tortor ac semper eleifend, dui magna consequat risus, eget cursus metus mi sollicitudin massa. Aenean erat augue, facilisis ac feugiat at, dictum sed turpis. Morbi id dolor at diam tincidunt tristique. Mauris feugiat, est quis interdum facilisis, augue nisi dignissim justo, eget laoreet mi risus sed augue. Nulla eleifend pulvinar turpis, non scelerisque augue suscipit non. Fusce sit amet vehicula lorem.</p>
